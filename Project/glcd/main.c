7f4ef313c796af3abe9f0055e84dc925e77a06e1
rdemeter <rdemeter@unitbv.ro>
2018-12-11 23:50:49 +0200

    added touch pen to draw on ILI9328

--- Project/glcd/main.c
+++ Project/glcd/main.c
@@ -3,6 +3,8 @@
 #include <stm32f10x_rcc.h>
 #include "delay.h"
 #include "lcd.h"
+#include "touch.h"
+
 #include "font.h"
 
 int main(void)
@@ -11,22 +13,21 @@ int main(void)
     delay_init(24);
 
     LCD_Init();
-
+    TP_Init();
     LCD_BL_SET;
-    LCD_Clear(ORANGE); // BLACK
+    TouchPanel_Calibrate();
 
-    //LCD_Rect(0,0,320,240,RGB565(255,255,255));
-    //LCD_Fill(0,0,50,100,ORANGE);
-    //LCD_DrawFillRectangle(0,10,100,10);
-    //LCD_DrawPoint(12,10);
-    //LCD_DrawPoint(13,10);
-    //LCD_DrawRectangle(20,20,200,200);
+    LCD_Clear(ORANGE);
 
-    //delay_ms(1000);
-    //LCD_DrawRectangle(40,20,220,220);
-    //LCD_DrawString(50,100,WHITE,YELLOW,"STM32F103RCT6",0,&Font8x8);
+    LCD_DrawRectangle(20,20,200,200);
+
+    LCD_DrawString(50,100,WHITE,YELLOW,"STM32F103RCT6",0,&Font8x8);
     while(1)
     {
-        
+        if(!GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_2))
+        {
+            getDisplayPoint(&display, Read_Ads7846(), &matrix ) ;
+            LCD_DrawString(display.x,display.y,WHITE,YELLOW,"*",0,&Font8x8);
+        }
     }
 }
