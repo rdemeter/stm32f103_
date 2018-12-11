b9f7f9230fe5e8f32b477e88dab39a69c05da603
rdemeter <rdemeter@unitbv.ro>
2018-12-11 23:29:48 +0200

    add lcd ILI9328 original driver from STM32_TFT_ILI9341_8BIT_DEMO_EMCU.zip project

---
+++ Project/glcd/lcd.c
@@ -0,0 +1,312 @@
+#include "lcd.h"
+#include "stdlib.h"
+#include "delay.h"	 
+
+_lcd_dev lcddev;
+
+u16 POINT_COLOR = 0x0000,BACK_COLOR = 0xFFFF;
+u16 DeviceCode;
+
+/*****************************************************************/
+void LCD_WR_REG(u8 data)
+{ 
+#if LCD_USE8BIT_MODEL==1
+	LCD_RS_CLR;
+	LCD_CS_CLR;
+	DATAOUT(data<<8);
+	LCD_WR_CLR;
+	LCD_WR_SET;
+	LCD_CS_SET;
+#else
+	LCD_RS_CLR;
+	LCD_CS_CLR;
+	DATAOUT(data);
+	LCD_WR_CLR;
+	LCD_WR_SET;
+	LCD_CS_SET;
+#endif	
+}
+
+/*****************************************************************/
+void LCD_WR_DATA(u16 data)
+{
+#if LCD_USE8BIT_MODEL==1
+	LCD_RS_SET;
+	LCD_CS_CLR;
+	DATAOUT(data<<8);
+	LCD_WR_CLR;
+	LCD_WR_SET;
+	LCD_CS_SET;
+	
+#else
+	LCD_RS_SET;
+	LCD_CS_CLR;
+	DATAOUT(data);
+	LCD_WR_CLR;
+	LCD_WR_SET;
+	LCD_CS_SET;
+#endif
+}
+
+/*****************************************************************/
+void LCD_DrawPoint_16Bit(u16 color)
+{
+#if LCD_USE8BIT_MODEL==1
+	LCD_CS_CLR;
+	LCD_RD_SET;
+	LCD_RS_SET;	 
+	DATAOUT(color);	
+	LCD_WR_CLR;
+	LCD_WR_SET;	
+	DATAOUT(color<<8);	
+	LCD_WR_CLR;
+	LCD_WR_SET;	 
+	LCD_CS_SET;
+#else
+	LCD_WR_DATA(color); 
+#endif
+}
+
+/*****************************************************************/
+void LCD_WriteReg(u8 LCD_Reg, u16 LCD_RegValue)
+{	
+	LCD_WR_REG(LCD_Reg);  
+	LCD_WR_DATA(LCD_RegValue);	    		 
+}	   
+	 
+/*****************************************************************/
+void LCD_WriteRAM_Prepare(void)
+{
+	LCD_WR_REG(lcddev.wramcmd);
+}	 
+
+/*****************************************************************/
+void LCD_DrawPoint(u16 x,u16 y)
+{
+	LCD_SetCursor(x,y);
+#if LCD_USE8BIT_MODEL==1
+	LCD_CS_CLR;
+	LCD_RD_SET;
+	LCD_RS_SET;
+	DATAOUT(POINT_COLOR);	
+	LCD_WR_CLR;
+	LCD_WR_SET;	
+	DATAOUT(POINT_COLOR<<8);	
+	LCD_WR_CLR;
+	LCD_WR_SET;	 
+	LCD_CS_SET;
+#else
+	LCD_WR_DATA(POINT_COLOR); 
+#endif
+}
+
+/*****************************************************************/
+void LCD_Clear(u16 Color)
+{
+	u32 index=0;      
+	LCD_SetWindows(0,0,lcddev.width-1,lcddev.height-1);	
+#if LCD_USE8BIT_MODEL==1
+	LCD_RS_SET;
+	LCD_CS_CLR;	   
+	for(index=0;index<lcddev.width*lcddev.height;index++)
+	{
+		DATAOUT(Color);	
+		LCD_WR_CLR;
+		LCD_WR_SET;	
+		
+		DATAOUT(Color<<8);	
+		LCD_WR_CLR;
+		LCD_WR_SET;	   
+	}
+	LCD_CS_SET;	
+#else
+	for(index=0;index<lcddev.width*lcddev.height;index++)
+	{
+		LCD_WR_DATA(Color);		  
+	}
+#endif
+} 
+
+/*****************************************************************/
+void LCD_GPIOInit(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+ 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC|RCC_APB2Periph_GPIOB|RCC_APB2Periph_AFIO, ENABLE); 
+	GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable , ENABLE);
+	
+	GPIO_InitStructure.GPIO_Pin = 1<<LCD_CS_PIN|1<<LCD_RS_PIN|1<<LCD_WR_PIN|1<<LCD_RD_PIN|1<<LCD_RST_PIN;	   //GPIO_Pin_10
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOC, &GPIO_InitStructure); //GPIOC	
+	GPIO_SetBits(GPIOC,1<<LCD_CS_PIN|1<<LCD_RS_PIN|1<<LCD_WR_PIN|1<<LCD_RD_PIN|1<<LCD_RST_PIN);
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_All;
+	GPIO_Init(GPIOB, &GPIO_InitStructure); //GPIOB
+	GPIO_SetBits(GPIOB,GPIO_Pin_All);	 
+}
+
+/*****************************************************************/
+void LCD_RESET(void)
+{
+	LCD_RST_CLR;
+	delay_ms(100);	
+	LCD_RST_SET;
+	delay_ms(50);
+}
+ 	 
+/*****************************************************************/
+void LCD_Init(void)
+{  
+	LCD_GPIOInit();
+ 	LCD_RESET();
+	//************* Start Initial Sequence **********//		
+	LCD_WR_REG(0xCF);  
+	LCD_WR_DATA(0x00); 
+	LCD_WR_DATA(0xC1); 
+	LCD_WR_DATA(0X30); 
+	LCD_WR_REG(0xED);  
+	LCD_WR_DATA(0x64); 
+	LCD_WR_DATA(0x03); 
+	LCD_WR_DATA(0X12); 
+	LCD_WR_DATA(0X81); 
+	LCD_WR_REG(0xE8);  
+	LCD_WR_DATA(0x85); 
+	LCD_WR_DATA(0x10); 
+	LCD_WR_DATA(0x7A); 
+	LCD_WR_REG(0xCB);  
+	LCD_WR_DATA(0x39); 
+	LCD_WR_DATA(0x2C); 
+	LCD_WR_DATA(0x00); 
+	LCD_WR_DATA(0x34); 
+	LCD_WR_DATA(0x02); 
+	LCD_WR_REG(0xF7);  
+	LCD_WR_DATA(0x20); 
+	LCD_WR_REG(0xEA);  
+	LCD_WR_DATA(0x00); 
+	LCD_WR_DATA(0x00); 
+	LCD_WR_REG(0xC0);    //Power control 
+	LCD_WR_DATA(0x1B);   //VRH[5:0] 
+	LCD_WR_REG(0xC1);    //Power control 
+	LCD_WR_DATA(0x01);   //SAP[2:0];BT[3:0] 
+	LCD_WR_REG(0xC5);    //VCM control 
+	LCD_WR_DATA(0x30); 	 //3F
+	LCD_WR_DATA(0x30); 	 //3C
+	LCD_WR_REG(0xC7);    //VCM control2 
+	LCD_WR_DATA(0XB7); 
+	LCD_WR_REG(0x36);    // Memory Access Control 
+	LCD_WR_DATA(0x48); 
+	LCD_WR_REG(0x3A);   
+	LCD_WR_DATA(0x55); 
+	LCD_WR_REG(0xB1);   
+	LCD_WR_DATA(0x00);   
+	LCD_WR_DATA(0x1A); 
+	LCD_WR_REG(0xB6);    // Display Function Control 
+	LCD_WR_DATA(0x0A); 
+	LCD_WR_DATA(0xA2); 
+	LCD_WR_REG(0xF2);    // 3Gamma Function Disable 
+	LCD_WR_DATA(0x00); 
+	LCD_WR_REG(0x26);    //Gamma curve selected 
+	LCD_WR_DATA(0x01); 
+	LCD_WR_REG(0xE0);    //Set Gamma 
+	LCD_WR_DATA(0x0F); 
+	LCD_WR_DATA(0x2A); 
+	LCD_WR_DATA(0x28); 
+	LCD_WR_DATA(0x08); 
+	LCD_WR_DATA(0x0E); 
+	LCD_WR_DATA(0x08); 
+	LCD_WR_DATA(0x54); 
+	LCD_WR_DATA(0XA9); 
+	LCD_WR_DATA(0x43); 
+	LCD_WR_DATA(0x0A); 
+	LCD_WR_DATA(0x0F); 
+	LCD_WR_DATA(0x00); 
+	LCD_WR_DATA(0x00); 
+	LCD_WR_DATA(0x00); 
+	LCD_WR_DATA(0x00); 		 
+	LCD_WR_REG(0XE1);    //Set Gamma 
+	LCD_WR_DATA(0x00); 
+	LCD_WR_DATA(0x15); 
+	LCD_WR_DATA(0x17); 
+	LCD_WR_DATA(0x07); 
+	LCD_WR_DATA(0x11); 
+	LCD_WR_DATA(0x06); 
+	LCD_WR_DATA(0x2B); 
+	LCD_WR_DATA(0x56); 
+	LCD_WR_DATA(0x3C); 
+	LCD_WR_DATA(0x05); 
+	LCD_WR_DATA(0x10); 
+	LCD_WR_DATA(0x0F); 
+	LCD_WR_DATA(0x3F); 
+	LCD_WR_DATA(0x3F); 
+	LCD_WR_DATA(0x0F); 
+	LCD_WR_REG(0x2B); 
+	LCD_WR_DATA(0x00);
+	LCD_WR_DATA(0x00);
+	LCD_WR_DATA(0x01);
+	LCD_WR_DATA(0x3f);
+	LCD_WR_REG(0x2A); 
+	LCD_WR_DATA(0x00);
+	LCD_WR_DATA(0x00);
+	LCD_WR_DATA(0x00);
+	LCD_WR_DATA(0xef);	 
+	LCD_WR_REG(0x11); //Exit Sleep
+	delay_ms(120);
+	LCD_WR_REG(0x29); //display on	
+
+	LCD_SetParam();
+	//LCD_LED=1;
+	//LCD_Clear(WHITE);
+}
+  		  
+
+/*****************************************************************/
+void LCD_SetWindows(u16 xStar, u16 yStar,u16 xEnd,u16 yEnd)
+{	
+	LCD_WR_REG(lcddev.setxcmd);	
+	LCD_WR_DATA(xStar>>8);
+	LCD_WR_DATA(0x00FF&xStar);		
+	LCD_WR_DATA(xEnd>>8);
+	LCD_WR_DATA(0x00FF&xEnd);
+	LCD_WR_REG(lcddev.setycmd);	
+	LCD_WR_DATA(yStar>>8);
+	LCD_WR_DATA(0x00FF&yStar);		
+	LCD_WR_DATA(yEnd>>8);
+	LCD_WR_DATA(0x00FF&yEnd);	
+	LCD_WriteRAM_Prepare();				
+}   
+
+/*****************************************************************/
+void LCD_SetCursor(u16 Xpos, u16 Ypos)
+{	  	    			
+	LCD_WR_REG(lcddev.setxcmd);	
+	LCD_WR_DATA(Xpos>>8);
+	LCD_WR_DATA(0x00FF&Xpos);		
+	LCD_WR_REG(lcddev.setycmd);	
+	LCD_WR_DATA(Ypos>>8);
+	LCD_WR_DATA(0x00FF&Ypos);		
+	LCD_WriteRAM_Prepare();
+} 
+
+/*****************************************************************/
+void LCD_SetParam(void)
+{ 	
+	lcddev.wramcmd=0x2C;
+#if USE_HORIZONTAL==1 
+	lcddev.dir=1;
+	lcddev.width=320;
+	lcddev.height=240;
+	lcddev.setxcmd=0x2A;
+	lcddev.setycmd=0x2B;			
+	LCD_WriteReg(0x36,0x6C);
+#else
+	lcddev.dir=0;		 	 		
+	lcddev.width=240;
+	lcddev.height=320;
+	lcddev.setxcmd=0x2A;
+	lcddev.setycmd=0x2B;	
+	LCD_WriteReg(0x36,0xC9);
+#endif
+}	
+
+
