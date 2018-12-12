7817af97ccf44728b2df3250ce4dbe39c5334a7f
rdemeter <35566820+rdemeter@users.noreply.github.com>
2018-12-12 16:26:58 +0200

    Update bounce_utils.h

--- Project/glcd/bounce_utils.h
+++ Project/glcd/bounce_utils.h
@@ -1,15 +1,16 @@
 #ifndef _BOUNCEUTILS_H_
 #define _BOUNCEUTILS_H_
+
 #include "touch.h" 
 #include "glcd.h"
 #include "delay.h"
 
 #define WIDTH		240
 #define HEIGHT		320
-#define CHAR_H   	8                  /* Character Height (in pixels)       */
-#define CHAR_W   	8                  /* Character Width (in pixels)        */
+#define CHAR_H   	8           /* Character Height (in pixels)       */
+#define CHAR_W   	8           /* Character Width (in pixels)        */
 #define BAR_W           10	    /* Bar Width (in pixels) */
-#define BAR_H		48					    /* Bar Height (in pixels) */
+#define BAR_H		48	    /* Bar Height (in pixels) */
 #define BAR_ON		1
 #define BAR_OFF		0
 #define T_LONG		10000000
@@ -20,25 +21,22 @@
 enum  pMode {SGL, DUAL, AI};
 
 typedef struct {
-	  int x;
-	  int y;
-	} PaddleInfo;
+  int x;
+  int y;
+} PaddleInfo;
 
 typedef struct {
-	  int dirn;
-	  int x; 
-	  int y; 
-	} BallInfo;
+  int dirn;
+  int x; 
+  int y; 
+} BallInfo;
 
 typedef struct {
-	unsigned long delay;
-	enum pMode mode;
- 	PaddleInfo p1;
-	PaddleInfo p2;
-	BallInfo ball;
-  } GameInfo;
-
-
-
+  unsigned long delay;
+  enum pMode mode;
+  PaddleInfo p1;
+  PaddleInfo p2;
+  BallInfo ball;
+} GameInfo;
 
 #endif
